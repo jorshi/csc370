74f8c75420af53f908d7f52fdee51a922cd6df57
adarguy <adarguy10@gmail.com>
2016-06-03 13:52:40 -0700

    CSC 370 Master
    added Accounts and Subsaidits tables

--- db_schema.sql
+++ db_schema.sql
@@ -3,6 +3,18 @@ create database subsaidit;
 
 use subsaidit;
 
-create table accounts(
-    username VARCHAR(255)
+create table Accounts(
+	username VARCHAR(255) PRIMARY KEY,
+	password CHAR(20) NOT NULL,
+	reputation INT DEFAULT 0
+);
+
+create table Subsaidits(
+	title VARCHAR(255) PRIMARY KEY,
+	description VARCHAR(512),
+	creation_time DATETIME DEFAULT NOW(),
+	front_page BOOLEAN DEFAULT 0,
+	creator_key VARCHAR(255) NOT NULL,
+	
+	FOREIGN KEY (creator_key) REFERENCES Accounts(username) ON DELETE CASCADE
 );
