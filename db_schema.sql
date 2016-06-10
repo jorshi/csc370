26eb2cc0048df2778f7bfc1d7dfe90738691f5a3
jorshi <jordieshier@gmail.com>
2016-06-09 18:57:37 -0700

    CSC 370 Project
    Added the posts, friends and subscribes create table statements.
    Changed the db name to saidit cus I think that makes more sense.

--- db_schema.sql
+++ db_schema.sql
@@ -1,7 +1,12 @@
-drop database subsaidit;
-create database subsaidit;
+# CSC 370 Project Schema
+# Adar Guy
+# Kyle Newman
+# Jordie Shier
 
-use subsaidit;
+DROP DATABASE IF EXISTS saidit;
+CREATE DATABASE saidit;
+
+USE saidit;
 
 create table Accounts(
 	username VARCHAR(255) PRIMARY KEY,
@@ -15,8 +20,24 @@ create table Subsaidits(
 	creation_time DATETIME DEFAULT NOW(),
 	front_page BOOLEAN DEFAULT 0,
 	creator_key VARCHAR(255) NOT NULL,
-	
+
 	FOREIGN KEY (creator_key) REFERENCES Accounts(username) ON DELETE CASCADE
+);
+
+CREATE TABLE posts (
+    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    publish_time DATETIME DEFAULT NOW(),
+    edit_time DATETIME DEFAULT NOW(),
+    subsaidit VARCHAR(255) NOT NULL,
+    url VARCHAR(2048),
+    upvotes INT DEFAULT 0,
+    downvotes INT DEFAULT 0,
+    text BLOB,
+
+    FOREIGN KEY (subsaidit) REFERENCES Subsaidits(title) ON DELETE CASCADE,
+    FOREIGN KEY (author) REFERENCES Accounts(username) ON DELETE CASCADE
 );
 
 create table Comments(
@@ -26,13 +47,31 @@ create table Comments(
     downvotes INT,
     creation_time DATETIME DEFAULT NOW(),
     text VARCHAR(1024),
-    
+
     FOREIGN KEY (commentor_id) REFERENCES Accounts(username) ON DELETE CASCADE
 );
 
 create table Favourites(
     user_id VARCHAR(255),
     post_id VARCHAR(255),
-    
+
     FOREIGN KEY (user_id) REFERENCES Accounts(username) ON DELETE CASCADE
+);
+
+CREATE TABLE friends (
+    user_id VARCHAR(255) NOT NULL,
+    friend_id VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (user_id,friend_id),
+    FOREIGN KEY (user_id) REFERENCES Accounts(username) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES Accounts(username) ON DELETE CASCADE
+);
+
+CREATE TABLE subscribes (
+    user_id VARCHAR(255) NOT NULL,
+    subsaid_id VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY (user_id, subsaid_id),
+    FOREIGN KEY (user_id) REFERENCES Accounts(username) ON DELETE CASCADE,
+    FOREIGN KEY (subsaid_id) REFERENCES Subsaidits(title) ON DELETE CASCADE
 );
