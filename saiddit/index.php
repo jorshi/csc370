a6fdae9d9936b87134aee923ee6820ad1a5058f0
jorshi <jordieshier@gmail.com>
2016-07-11 14:34:27 -0700

    CSC370 Saiddit
    Added session login & logout

--- saiddit/index.php
+++ saiddit/index.php
@@ -1,31 +1,22 @@
 <?php
-include('login.php');
-
-if (isset($_SESSION['login_user'])) {
-    header("location: profile.php");
-}
+session_start();
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Saiddit Login</title>
+        <title>Saiddit</title>
         <link href="static/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div id="main">
             <h1>Saiddit Login</h1>
-            <div id="login">
-                <h2>Login Form</h2>
-                <form action="" method="post">
-                    <label>UserName :</label>
-                    <input id="name" name="username" placeholder="username" type="text">
-                    <label>Password :</label>
-                    <input id="password" name="password" placeholder="**********" type="password">
-                    <input name="submit" type="submit" value=" Login ">
-                    <span><?php echo $error; ?></span>
-                </form>
-            </div>
+            <?php
+                if(isset($_SESSION['login_user'])) {
+                    echo "<a href='logout.php'>logout</a>";
+                } else {
+                    echo "<a href='login.php'>login</a>";
+                }
+            ?>
         </div>
     </body>
 </html>
--- saiddit/login.php
+++ saiddit/login.php
@@ -5,6 +5,10 @@ include('db_utils/connect.php');
 session_start();
 $error = '';
 
+if (isset($_SESSION['login_user'])) {
+    header("location: index.php");
+}
+
 // Check to make sure the username and password fields have been filled
 if (isset($_POST['submit'])) {
     if(empty($_POST['username']) || empty($_POST['password'])) {
@@ -36,7 +40,7 @@ if (isset($_POST['submit'])) {
             // If user was found then log them in and store user session
             if ($rows == 1) {
                 $_SESSION['login_user'] = $username;
-                header("locaion: profile.php");
+                header("location: index.php");
             } else {
                 $error = "Invalid username or password";
             }
@@ -46,3 +50,27 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Saiddit Login</title>
+        <link href="static/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="main">
+            <h1>Saiddit Login</h1>
+            <div id="login">
+                <h2>Login Form</h2>
+                <form action="" method="post">
+                    <label>UserName :</label>
+                    <input id="name" name="username" placeholder="username" type="text">
+                    <label>Password :</label>
+                    <input id="password" name="password" placeholder="**********" type="password">
+                    <input name="submit" type="submit" value=" Login ">
+                    <span><?php echo $error; ?></span>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
