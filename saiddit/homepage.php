9e46277820a077839053792e22a4c9acbba93a88
KYLE <newmankyleSWL@gmail.com>
2016-07-16 23:43:36 -0700

    added new_subsaiddit.php. An empty form right now, based off of new_post.php

--- saiddit/homepage.php
+++ saiddit/homepage.php
@@ -54,7 +54,8 @@
                 </div>
                 <div id='ads'>
                     <button class='new' id='new_link' >Submit a new link</button><br><br>
-                    <button class='new' id='new_post' onclick='add_new()'>Submit a new text post</button>
+                    <button class='new' id='new_post' onclick='add_new()'>Submit a new text post</button><br><br>
+                    <button class='new' id='new_post' >Create your own Subsaiddit!</button>
                 </div>
             </div>
 
