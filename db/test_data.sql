af9db0e15d62eb90de8299d26a8379f79bb2a448
KYLE <newmankyleSWL@gmail.com>
2016-07-19 21:29:49 -0700

    changed test_data.sql for queries

--- db/test_data.sql
+++ db/test_data.sql
@@ -12,6 +12,10 @@ REPLACE INTO accounts(username, password) VALUES
     ('steve12', SHA2(CONCAT('a1b2c3', @salt), 256)),
     ('test', SHA2(CONCAT('abcd', @salt), 256));
     
+REPLACE INTO subsaiddits(title, description, front_page, creator_key) VALUES
+    ('funny', 'just a funny subsaiddit', TRUE, 'test'),
+    ('pics', 'a subsaiddit about pics', TRUE, 'john');
+    
 REPLACE INTO friends(user_id, friend_id) VALUES ('test', 'john');
     
 REPLACE INTO posts(title, author, subsaiddit, url, upvotes, downvotes, text) VALUES
@@ -20,4 +24,4 @@ REPLACE INTO posts(title, author, subsaiddit, url, upvotes, downvotes, text) VAL
     ('friend\'s post', 'john', 'pics', 'arbitrary url', 10, 0, 'to be, or not to be'),
     ('final post!', 'john', 'pics', 'arbitrary url', 0, -12, 'to be, or not to be');
     
-REPLACE INTO favourites(user_id, post_id) VALUES ('john', 661);
+REPLACE INTO favourites(user_id, post_id) VALUES ('test', 1);
