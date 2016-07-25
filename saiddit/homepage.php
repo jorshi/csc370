9bc414f32ef3add70f0f2218bd95e27a51ad4670
KYLE <newmankyleSWL@gmail.com>
2016-07-24 19:11:12 -0700

    added subcribe.js

--- saiddit/homepage.php
+++ saiddit/homepage.php
@@ -96,28 +96,5 @@
     </body>
 </html>
 
-<script>
 
-    function subscribeTo(_element) {
-        $.ajax({
-            url: 'db_utils/subscribe_button.php',
-            data: {action: '<?php echo $hidesubscribe ?>', subsaiddit: '<?php echo $subsaiddit ?>'},
-            type: 'post',
-            success: function() {
-                if ($(_element).hasClass('subscribe')){
-                    $(_element).removeClass("subscribe");
-                    $(_element).toggleClass("unsubscribe");
-                    $(_element).text("unsubscribe");
-                }else{
-                    $(_element).removeClass("unsubscribe");
-                    $(_element).addClass("subscribe");
-                    $(_element).text("subscribe");
-                }
-            },
-            error: function(xhr, error) {
-                alert('Holy errors batman!');
-            }
-        });
-    }
-</script>
 
