31f2672df180801d1484e8ce8177a25d6d1ea3a9
KYLE <newmankyleSWL@gmail.com>
2016-07-24 23:06:52 -0700

    added more to test_data and refined the step 8 queries

--- db/test_data.sql
+++ db/test_data.sql
@@ -14,14 +14,27 @@ REPLACE INTO accounts(username, password) VALUES
     
 REPLACE INTO subsaiddits(title, description, front_page, creator_key) VALUES
     ('funny', 'just a funny subsaiddit', TRUE, 'test'),
+    ('politics', 'about politics', TRUE, 'test'),
+    ('webdev', 'hunting', 0, 'test'),
+    ('angry twitter', 'a subsaiddit about twitter', FALSE, 'test'),
     ('pics', 'a subsaiddit about pics', TRUE, 'john');
     
-REPLACE INTO friends(user_id, friend_id) VALUES ('test', 'john');
+REPLACE INTO friends(user_id, friend_id) VALUES 
+    ('test', 'john'),
+    ('john', 'test');
     
 REPLACE INTO posts(title, author, subsaiddit, url, upvotes, downvotes, text) VALUES
     ('test post, please ignore', 'test', 'funny', 'arbitrary url', 10, 0, 'you ever wonder why we\'re here?'),
-    ('second post', 'test', 'funny', 'arbitrary url', 0, -110, 'much ado about nothing.'),
+    ('second test post', 'test', 'funny', 'arbitrary url', 0, -110, 'much ado about nothing.'),
     ('friend\'s post', 'john', 'pics', 'arbitrary url', 10, 0, 'to be, or not to be'),
-    ('final post!', 'john', 'pics', 'arbitrary url', 0, -12, 'to be, or not to be');
+    ('final post from john!', 'john', 'pics', 'arbitrary url', 0, -12, 'to be, or not to be');
     
-REPLACE INTO favourites(user_id, post_id) VALUES ('test', 1);
+REPLACE INTO favourites(user_id, post_id) VALUES 
+    ('test', 1),
+    ('john', 2);
+
+REPLACE INTO subscribes(user_id, subsaid_id) VALUES
+    ('john', 'pics'),
+    ('john', 'webdev'),
+    ('test', 'funny'),
+    ('test', 'angry twitter');
